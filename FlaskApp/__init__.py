f8965453b30c0bd68c634cf08f0a1eebcaca7bfc
amharF <amhar.ford@gmail.com>
2016-04-29 16:53:09 +0000

    modified:   __init__.py 	modified:   templates/header.html

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -131,7 +131,7 @@ def gconnect():
     output += login_session['picture']
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-\
     webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-    flash("you are now logged in as %s" % login_session['username'])
+    flash("you are now logged in as %s" % login_session['email'])
     print "done!"
     return output
 
